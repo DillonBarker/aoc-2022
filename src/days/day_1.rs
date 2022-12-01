c55ccd8470aa1c6aa53c9ea5f1a946a13122da0b
dillon.barker <dillon.barker@testevolve.com>
2022-12-01 15:52:25 +0000

    adding unit tests using smaller test data from problem

--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -82,3 +82,32 @@ fn solve_b(lines: Vec<String>) -> i32  {
 	println!("Day 1 part b: {}", total_cal);
 	return total_cal;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+
+
+    #[test]
+    fn solve_a_returns_count() {
+        let test_input: Vec<String> = vec!["1000".to_string(), "2000".to_string(), 
+            "3000".to_string(), "".to_string(), "4000".to_string(), "".to_string(), 
+            "5000".to_string(), "6000".to_string(), "".to_string(), "7000".to_string(), 
+            "8000".to_string(), "9000".to_string(), "".to_string(), "10000".to_string()];
+
+        let result = solve_a(test_input);
+        assert_eq!(result, 24000);
+    }
+
+    #[test]
+    fn solve_b_returns_count() {
+        let test_input: Vec<String> = vec!["1000".to_string(), "2000".to_string(), 
+            "3000".to_string(), "".to_string(), "4000".to_string(), "".to_string(), 
+            "5000".to_string(), "6000".to_string(), "".to_string(), "7000".to_string(), 
+            "8000".to_string(), "9000".to_string(), "".to_string(), "10000".to_string()];
+
+        let result = solve_b(test_input);
+        assert_eq!(result, 41000);
+    }
+}
